ee0eb3ee3f856a054ed160ae0ef30cd93757a07a
d5ng <dong5ffice@gmail.com>
2025-07-29 15:34:59 +0900

    chore: tsconfig and viteconfig 테스트 환경 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+/// <reference types="vitest" />
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: false,
+    environment: "jsdom"
+  }
 })
