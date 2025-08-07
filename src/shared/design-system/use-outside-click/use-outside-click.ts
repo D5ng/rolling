c969c61fae64a9f7f2527ffeb279e50dd7616a45
d5ng <dong5ffice@gmail.com>
2025-08-07 14:00:20 +0900

    feat(hooks): useOutsideClick hook 구현

---
+++ src/shared/design-system/use-outside-click/use-outside-click.ts
@@ -0,0 +1,31 @@
+import { useEffect, type RefObject } from "react"
+
+export default function useOutsideClick<TElement extends HTMLElement>(
+  refs: RefObject<TElement> | RefObject<TElement>[],
+  callback: () => void,
+  enabled: boolean
+) {
+  useEffect(() => {
+    if (!enabled) {
+      return
+    }
+
+    const refArray = Array.isArray(refs) ? refs : [refs]
+
+    const listener = (event: MouseEvent | TouchEvent) => {
+      const isClickInside = refArray.some((ref) => ref.current?.contains(event.target as Node))
+
+      if (!isClickInside) {
+        callback()
+      }
+    }
+
+    document.addEventListener("mousedown", listener)
+    document.addEventListener("touchstart", listener)
+
+    return () => {
+      document.removeEventListener("mousedown", listener)
+      document.removeEventListener("touchstart", listener)
+    }
+  }, [refs, callback, enabled])
+}
