f70a8184768ad1626f767334a943ba7d3c50d390
d5ng <dong5ffice@gmail.com>
2025-08-09 02:09:10 +0900

    refactor(textField): 타입 리팩토링

--- src/shared/design-system/text-field/text-field.tsx
+++ src/shared/design-system/text-field/text-field.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable func-call-spacing */
-/* eslint-disable indent */
 import { forwardRef, type InputHTMLAttributes, type Ref, type TextareaHTMLAttributes } from "react"
 
 import { cn } from "@/shared/utils"
@@ -8,42 +6,43 @@ import Input from "../input/input"
 import Label from "../label/label"
 import Textarea from "../textarea/textarea"
 
-interface Props {
+interface BaseProps {
   label?: string
   as?: "input" | "textarea"
   error?: boolean
   disabled?: boolean
 }
 
-const TextField = forwardRef<
-  HTMLInputElement | HTMLTextAreaElement,
-  Props & (InputHTMLAttributes<HTMLInputElement> | TextareaHTMLAttributes<HTMLTextAreaElement>)
->(({ className, id, label, as = "input", error, disabled, ...restProps }, ref) => {
-  return (
-    <div className={cn(`flex flex-col gap-3`, className)}>
-      {label && <Label htmlFor={id}>{label}</Label>}
-      {as === "input" && (
-        <Input
-          type="text"
-          id={id}
-          error={error}
-          disabled={disabled}
-          ref={ref as Ref<HTMLInputElement>}
-          {...(restProps as InputHTMLAttributes<HTMLInputElement>)}
-        />
-      )}
-      {as === "textarea" && (
-        <Textarea
-          id={id}
-          error={error}
-          disabled={disabled}
-          ref={ref as Ref<HTMLTextAreaElement>}
-          {...(restProps as TextareaHTMLAttributes<HTMLTextAreaElement>)}
-        />
-      )}
-    </div>
-  )
-})
+type Props = BaseProps & (InputHTMLAttributes<HTMLInputElement> | TextareaHTMLAttributes<HTMLTextAreaElement>)
+
+const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Props>(
+  ({ className, id, label, as = "input", error, disabled, ...restProps }, ref) => {
+    return (
+      <div className={cn(`flex flex-col gap-3`, className)}>
+        {label && <Label htmlFor={id}>{label}</Label>}
+        {as === "input" && (
+          <Input
+            type="text"
+            id={id}
+            error={error}
+            disabled={disabled}
+            ref={ref as Ref<HTMLInputElement>}
+            {...(restProps as InputHTMLAttributes<HTMLInputElement>)}
+          />
+        )}
+        {as === "textarea" && (
+          <Textarea
+            id={id}
+            error={error}
+            disabled={disabled}
+            ref={ref as Ref<HTMLTextAreaElement>}
+            {...(restProps as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+          />
+        )}
+      </div>
+    )
+  }
+)
 
 TextField.displayName = "TextField"
 
