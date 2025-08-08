320e8fa9ce7e5b73a249e61385c1a83d3dce8f29
d5ng <dong5ffice@gmail.com>
2025-08-08 20:44:13 +0900

    feat(avatar): Avatar 컴포넌트 추가

---
+++ src/shared/design-system/avatar/avatar-fallback.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/shared/utils"
+
+import { useAvatar } from "./avatar-context"
+
+import type { HTMLAttributes } from "react"
+
+export default function AvatarFallback({ children, className, ...restProps }: HTMLAttributes<HTMLDivElement>) {
+  const { imageLoadingStatus } = useAvatar()
+
+  return imageLoadingStatus !== "loaded" ? (
+    <div className={cn("text-gray-500", className)} {...restProps}>
+      {children}
+    </div>
+  ) : null
+}
