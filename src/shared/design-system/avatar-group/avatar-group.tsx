9d9f1c12b9be238b48e52671fdc3176715b27e93
d5ng <dong5ffice@gmail.com>
2025-08-11 17:57:23 +0900

    feat(AvatarGroup): renderSurplus Prop 추가 및 리팩토링

--- src/shared/design-system/avatar-group/avatar-group.tsx
+++ src/shared/design-system/avatar-group/avatar-group.tsx
@@ -5,22 +5,51 @@ import { cn } from "@/shared/utils"
 import Avatar from "../avatar/avatar"
 import AvatarFallback from "../avatar/avatar-fallback"
 
+const MAX_AVATAR_COUNT = 99
+
 interface Props {
   total?: number
-  children: ReactNode
+  renderSurplus?: (count: number) => ReactNode
   className?: string
+  children: ReactNode
 }
 
-export default function AvatarGroup({ total = 0, children, className }: Props) {
-  const avatarCount = Children.count(children)
-  const renderAvatarCount = total - avatarCount > 99 ? 99 : total - avatarCount
+export default function AvatarGroup({ total = 0, children, className, renderSurplus }: Props) {
+  const renderedCount = Children.count(children)
+  const remainingCount = calculateRemainingCount(total, renderedCount)
 
   return (
     <div className={cn("flex -space-x-3", className)}>
       {children}
-      <Avatar className="w-7 h-7 bg-white border-white">
-        <AvatarFallback>+{renderAvatarCount}</AvatarFallback>
-      </Avatar>
+      <SurPlusAvatar count={remainingCount} renderSurplus={renderSurplus} />
     </div>
   )
+}
+
+function SurPlusAvatar({ count, renderSurplus }: { count: number; renderSurplus?: (count: number) => ReactNode }) {
+  if (count <= 0) {
+    return null
+  }
+
+  return (
+    renderSurplus?.(count) ?? (
+      <Avatar className="w-7 h-7 bg-white border-white">
+        <AvatarFallback>+{count}</AvatarFallback>
+      </Avatar>
+    )
+  )
+}
+
+function calculateRemainingCount(total: number, count: number) {
+  const remainingCount = total - count
+
+  if (remainingCount <= 0) {
+    return 0
+  }
+
+  if (remainingCount > MAX_AVATAR_COUNT) {
+    return MAX_AVATAR_COUNT
+  }
+
+  return remainingCount
 }
