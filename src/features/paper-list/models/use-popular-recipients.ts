7d7a82a1df3507fd9291da1bdb4a5c4ee2c2a9a4
d5ng <dong5ffice@gmail.com>
2025-08-15 22:40:10 +0900

    refactor(feature): paper-list public api 구성 및 import문 변경

--- src/features/paper-list/models/use-popular-recipients.ts
+++ src/features/paper-list/models/use-popular-recipients.ts
@@ -2,9 +2,9 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 
 import { recipientQueryKey, RECIPIENTS_LIMIT, RECIPIENTS_OFFSET } from "@/entities/recipient"
 
-import { getRecipients } from "../apis"
+import { getRecipients } from "../apis/get-recipients"
 
-import type { RecipientsParams } from "../apis"
+import type { RecipientsParams } from "../apis/types"
 import type { Recipient } from "@/entities/recipient"
 
 export function usePopularRecipients({ limit = RECIPIENTS_LIMIT, offset = RECIPIENTS_OFFSET }: RecipientsParams) {
