09cac30960d23e63db3967ffa8cc88b4670f73e7
d5ng <dong5ffice@gmail.com>
2025-08-15 22:16:35 +0900

    refactor: 자잘한 수정

--- src/features/paper-list/ui/popular-paper.tsx
+++ src/features/paper-list/ui/popular-paper.tsx
@@ -1,10 +1,12 @@
-import { useRecipients } from "../models"
+import { RECIPIENTS_LIMIT, RECIPIENTS_OFFSET } from "@/entities/recipient"
+
+import { usePopularRecipients } from "../models"
 
 import PaperList from "./paper-list"
 import PaperSection from "./paper-section"
 
 export default function PopularPaper() {
-  const { data } = useRecipients({ type: "popular", limit: 100, offset: 0 })
+  const { data } = usePopularRecipients({ limit: RECIPIENTS_LIMIT, offset: RECIPIENTS_OFFSET })
 
   return (
     <PaperSection renderTitle="인기 롤링 페이퍼 🔥">
--- src/features/paper-list/ui/recent-paper.tsx
+++ src/features/paper-list/ui/recent-paper.tsx
@@ -1,14 +1,16 @@
-import { RECIPIENTS_LIMIT, RECIPIENTS_OFFSET, useRecipients } from "../models"
+import { RECIPIENTS_LIMIT, RECIPIENTS_OFFSET } from "@/entities/recipient"
+
+import { useRecentRecipients } from "../models"
 
 import PaperList from "./paper-list"
 import PaperSection from "./paper-section"
 
 export default function RecentPaper() {
-  const { data: recipientsData } = useRecipients({ type: "recent", limit: RECIPIENTS_LIMIT, offset: RECIPIENTS_OFFSET })
+  const { data } = useRecentRecipients({ limit: RECIPIENTS_LIMIT, offset: RECIPIENTS_OFFSET })
 
   return (
     <PaperSection renderTitle="최근에 만든 롤링 페이퍼 ⭐️️">
-      <PaperList recipients={recipientsData!.results} />
+      <PaperList recipients={data!.results} />
     </PaperSection>
   )
 }
