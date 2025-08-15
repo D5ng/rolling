7fc755417889fb159c7511fbc9c546279c2d2bc9
d5ng <dong5ffice@gmail.com>
2025-08-15 22:14:27 +0900

    refactor: type 리팩토링 및 get-recipients 예외처리

--- src/features/paper-list/apis/get-recipients.ts
+++ src/features/paper-list/apis/get-recipients.ts
@@ -1,17 +1,24 @@
-import { axiosInstance } from "@/shared/config"
+import { AxiosError } from "axios"
 
-import { RECIPIENTS_LIMIT, RECIPIENTS_OFFSET } from "../models/paper-list.constants"
+import { RECIPIENTS_LIMIT, RECIPIENTS_OFFSET } from "@/entities/recipient"
+import { axiosInstance } from "@/shared/config"
 
 import type { RecipientsParams } from "./types"
 import type { RecipientsResponse } from "@/entities/recipient"
 
 export async function getRecipients({ limit = RECIPIENTS_LIMIT, offset = RECIPIENTS_OFFSET }: RecipientsParams = {}) {
-  const { data } = await axiosInstance.get<RecipientsResponse>(`recipients/`, {
-    params: {
-      limit,
-      offset
-    }
-  })
+  try {
+    const { data } = await axiosInstance.get<RecipientsResponse>(`recipients/`, {
+      params: {
+        limit,
+        offset
+      }
+    })
 
-  return data
+    return data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error("롤링 페이퍼 목록을 불러오는데 실패했어요. 잠시 후 다시 시도해주세요.")
+    }
+  }
 }
--- src/features/paper-list/apis/types.ts
+++ src/features/paper-list/apis/types.ts
@@ -1,12 +1,4 @@
-type RECIPIENTS_TYPE = "recent" | "popular"
-
 export interface RecipientsParams {
-  limit?: number
-  offset?: number
-}
-
-export interface RecipientsQueryKey extends RecipientsParams {
-  type: RECIPIENTS_TYPE
   limit?: number
   offset?: number
 }
