90b7931d454a8d2a537a43103910cc4d433264e0
d5ng <dong5ffice@gmail.com>
2025-08-20 16:44:19 +0900

    feat(types): background color, image 타입 추가

--- src/entities/paper/paper.types.ts
+++ src/entities/paper/paper.types.ts
@@ -1,9 +1,12 @@
 // Cross-import 처리
 // https://feature-sliced.design/kr/docs/reference/public-api#public-api-for-cross-imports
+import type { PAPER_BACKGROUND_COLORS, PAPER_BACKGROUND_IMAGES } from "./paper.constants"
 import type { RecentMessages } from "@/entities/paper/@x/message"
 import type { Reaction } from "@/entities/paper/@x/reaction"
 
-export type BackgroundColor = "beige" | "purple" | "blue" | "green"
+export type BackgroundColor = (typeof PAPER_BACKGROUND_COLORS)[number]
+
+export type BackgroundImage = (typeof PAPER_BACKGROUND_IMAGES)[number]
 
 export interface PapersResponse {
   count: number
