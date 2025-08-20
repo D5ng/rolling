06635b01611c8534c34194c6095edbb3e445b222
d5ng <dong5ffice@gmail.com>
2025-08-20 11:17:23 +0900

    refactor(SegmentedControl): 마크업 변경

--- src/shared/design-system/segmented-control/segmented-control-list.tsx
+++ src/shared/design-system/segmented-control/segmented-control-list.tsx
@@ -2,15 +2,15 @@ import { cn } from "../../utils/cn"
 
 import type { HTMLAttributes } from "react"
 
-export default function SegmentedControlList({ children, className, ...restProps }: HTMLAttributes<HTMLUListElement>) {
+export default function SegmentedControlList({ children, className, ...restProps }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <ul
+    <div
       className={cn("w-fit flex items-center bg-gray-100 rounded-md", className)}
       {...restProps}
       role="tablist"
       aria-label="탭 목록"
     >
       {children}
-    </ul>
+    </div>
   )
 }
