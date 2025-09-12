01d01e3ccc8755d902732b1f9a79237781608d73
d5ng <dong5ffice@gmail.com>
2025-09-12 14:14:41 +0900

    design: header 반응형 스타일링 수정

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/shared/design-system/button"
 export default function Header() {
   return (
     <header className="sticky top-0 bg-white border-b border-gray-200">
-      <div className="flex items-center justify-between px-4 py-3 desktop:w-[1200px] desktop:mx-auto">
+      <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-3 desktop:px-0">
         <h1 className="h-[30px]">
           <Link to="/">
             <LogoIcon />
