c58bd49fa8f0cd7eef2101448b6732d3bd8588a3
d5ng <dong5ffice@gmail.com>
2025-09-12 15:26:56 +0900

    bugfix: z-index 50 추가

--- src/features/reaction/add-reaction/ui/add-reaction-button.tsx
+++ src/features/reaction/add-reaction/ui/add-reaction-button.tsx
@@ -26,7 +26,7 @@ export default function AddReactionButton({ id }: Props) {
   )
 
   return (
-    <div className="relative" ref={emojiRef}>
+    <div className="relative z-50" ref={emojiRef}>
       <Button variant="outlined" className="w-9 h-8 flex items-center justify-center" onClick={toggle}>
         <AddReactionIcon className="w-5 h-5" />
       </Button>
