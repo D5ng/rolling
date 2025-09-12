c2495ee6bd3b14e2132bc544c8803c1c68c29d54
d5ng <dong5ffice@gmail.com>
2025-09-12 15:27:01 +0900

    bugfix: z-index 50 추가

--- src/features/reaction/view-reactions/ui/view-reactions.tsx
+++ src/features/reaction/view-reactions/ui/view-reactions.tsx
@@ -10,7 +10,7 @@ export default function ViewReactions({ id }: Props) {
   const { data } = useReactionsQuery(id)
 
   return (
-    <div className="absolute p-4 bg-white rounded-2xl border border-gray-300 grid grid-cols-3 gap-x-2 gap-y-2.5 top-[calc(100%+10px)] left-0">
+    <div className="absolute p-4 bg-white rounded-2xl border border-gray-300 grid grid-cols-3 gap-x-2 gap-y-2.5 top-[calc(100%+10px)] left-0 z-50">
       {data.results.map((reaction) => (
         <ReactionBadge key={reaction.id} emoji={reaction.emoji} count={reaction.count} />
       ))}
