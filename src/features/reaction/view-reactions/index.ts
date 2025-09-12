c44928666ba704a2cf246bc90265fe219fff99bb
d5ng <dong5ffice@gmail.com>
2025-09-12 14:56:51 +0900

    feat: ViewReactions 컴포넌트 추가

---
+++ src/features/reaction/view-reactions/index.ts
@@ -0,0 +1,1 @@
+export { default as ViewReactionsButton } from "./ui/view-reactions-button"
---
+++ src/features/reaction/view-reactions/ui/view-reactions.tsx
@@ -0,0 +1,19 @@
+import { ReactionBadge } from "@/shared/design-system/reaction-badge"
+
+import { useReactionsQuery } from "../model/use-reactions.query"
+
+interface Props {
+  id: string | number
+}
+
+export default function ViewReactions({ id }: Props) {
+  const { data } = useReactionsQuery(id)
+
+  return (
+    <div className="absolute p-4 bg-white rounded-2xl border border-gray-300 grid grid-cols-3 gap-x-2 gap-y-2.5 top-[calc(100%+10px)] left-0">
+      {data.results.map((reaction) => (
+        <ReactionBadge key={reaction.id} emoji={reaction.emoji} count={reaction.count} />
+      ))}
+    </div>
+  )
+}
