2c9efa9f8b19e7a9481c61dbe9484e0db3749cda
d5ng <dong5ffice@gmail.com>
2025-09-19 23:26:22 +0900

    feat: message 엔터티 자잘한 수정

--- src/entities/message/index.ts
+++ src/entities/message/index.ts
@@ -1,1 +1,2 @@
 export type { MessagesResponse, Message, Font } from "./message.types"
+export { RELATION_SHIP_LISTS } from "./message.constants"
---
+++ src/entities/message/message.constants.ts
@@ -0,0 +1,3 @@
+export const RELATION_SHIP_LISTS = ["지인", "동료", "가족", "친구"] as const
+
+export const FONTS = ["Pretendard", "Noto Sans", "나눔명조", "나눔손글씨 손편지체"] as const
