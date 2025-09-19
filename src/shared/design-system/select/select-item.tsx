bc0b355318384053a347444914e9ef02a109a643
d5ng <dong5ffice@gmail.com>
2025-09-19 23:12:16 +0900

    refactor: children이 없고 value 값이 있다면 value를 렌더링하도록 수정

--- src/shared/design-system/select/select-item.tsx
+++ src/shared/design-system/select/select-item.tsx
@@ -54,7 +54,7 @@ export default function SelectItem({ children, value, ...restProps }: Props) {
       value={value}
       {...restProps}
     >
-      {children}
+      {children || value}
     </li>
   )
 }
