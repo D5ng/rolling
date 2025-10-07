5d380d452494e006256076d7870b94a54de5b91c
d5ng <dong5ffice@gmail.com>
2025-10-07 10:20:35 +0900

    feat: create-message mutation 훅 추가

--- src/pages/paper-detail/models/use-paper-messages.query.ts
+++ src/pages/paper-detail/models/use-paper-messages.query.ts
@@ -1,12 +1,24 @@
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query"
 
 import { paperQueryKey } from "@/entities/paper"
 
 import { getPaperMessages } from "../api/get-paper-messages"
 
 export function usePaperMessagesQuery(id: string | number) {
-  return useSuspenseQuery({
+  return useSuspenseInfiniteQuery({
     queryKey: paperQueryKey.messages(id),
-    queryFn: () => getPaperMessages(id)
+    queryFn: ({ pageParam = 0 }) => getPaperMessages(id, pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const offset = new URL(lastPage.next).searchParams.get("offset")
+        return Number(offset)
+      }
+
+      return undefined
+    },
+    select: (data) => {
+      return data.pages.flatMap((page) => page.results)
+    }
   })
 }
